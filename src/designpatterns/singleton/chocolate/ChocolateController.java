fbbfd938db6edd6527b976b388e968734d19b1b8
ReadnThink <103480627+ReadnThink@users.noreply.github.com>
2023-07-13 15:20:27 +0900

    싱글톤 패턴

---
+++ src/designpatterns/singleton/chocolate/ChocolateController.java
@@ -0,0 +1,13 @@
+package designpatterns.singleton.chocolate;
+
+public class ChocolateController {
+    public static void main(String[] args) {
+        ChocolateBoiler boiler = ChocolateBoiler.getInstance();
+        boiler.fill();
+        boiler.boil();
+        boiler.drain();
+
+        ChocolateBoiler boiler1 = ChocolateBoiler.getInstance();
+
+    }
+}
