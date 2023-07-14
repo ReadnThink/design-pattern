da98d3c509c08153db3008add91068b92124b1bb
ReadnThink <103480627+ReadnThink@users.noreply.github.com>
2023-07-14 15:55:27 +0900

    어댑터 패턴

---
+++ src/designpatterns/adapter/duck/adapters/DuckAdapter.java
@@ -0,0 +1,26 @@
+package designpatterns.adapter.duck.adapters;
+
+
+import designpatterns.adapter.duck.interfaces.Duck;
+
+import java.util.Random;
+
+public class DuckAdapter {
+    Duck duck;
+    Random random;
+
+    public DuckAdapter(final Duck duck) {
+        this.duck = duck;
+        random = new Random();
+    }
+
+    public void gobble() {
+        duck.quack();
+    }
+
+    public void fly() {
+        if (random.nextInt(5) == 0) {
+            duck.fly();
+        }
+    }
+}
---
+++ src/designpatterns/adapter/duck/interfaces/Duck.java
@@ -0,0 +1,7 @@
+package designpatterns.adapter.duck.interfaces;
+
+public interface Duck {
+    public void quack();
+
+    public void fly();
+}
