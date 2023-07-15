373526162fa77e0f74fb1eecee721e395cff545c
ReadnThink <103480627+ReadnThink@users.noreply.github.com>
2023-07-15 16:29:08 +0900

    facade 패턴

---
+++ src/designpatterns/facade/HomeTheaterFacade.java
@@ -0,0 +1,86 @@
+package designpatterns.facade;
+
+public class HomeTheaterFacade {
+    Amplifier amp;
+    Tuner tuner;
+    StreamingPlayer player;
+    Projector projector;
+    TheaterLights lights;
+    Screen screen;
+    PopcornPopper popper;
+
+    public HomeTheaterFacade(final Amplifier amp,
+                             final Tuner tuner,
+                             final StreamingPlayer player,
+                             final Projector projector,
+                             final TheaterLights lights,
+                             final Screen screen,
+                             final PopcornPopper popper) {
+        this.amp = amp;
+        this.tuner = tuner;
+        this.player = player;
+        this.projector = projector;
+        this.lights = lights;
+        this.screen = screen;
+        this.popper = popper;
+    }
+    public HomeTheaterFacade(Amplifier amp,
+                             Tuner tuner,
+                             StreamingPlayer player,
+                             Projector projector,
+                             Screen screen,
+                             TheaterLights lights,
+                             PopcornPopper popper) {
+
+        this.amp = amp;
+        this.tuner = tuner;
+        this.player = player;
+        this.projector = projector;
+        this.screen = screen;
+        this.lights = lights;
+        this.popper = popper;
+    }
+
+    public void watchMovie(String movie) {
+        System.out.println("영화볼 준비중...");
+        popper.on();
+        popper.pop();
+        lights.dim(10);
+        screen.down();
+        projector.on();
+        projector.wideScreenMode();
+        amp.on();
+        amp.setStreamingPlayer(player);
+        amp.setSurroundSound();
+        amp.setVolume(5);
+        player.on();
+        player.play(movie);
+    }
+
+
+    public void endMovie() {
+        System.out.println("홈 시어터 끄는중...");
+        popper.off();
+        lights.on();
+        screen.up();
+        projector.off();
+        amp.off();
+        player.stop();
+        player.off();
+    }
+
+    public void listenToRadio(double frequency) {
+        System.out.println("라도오 트는중...");
+        tuner.on();
+        tuner.setFrequency(frequency);
+        amp.on();
+        amp.setVolume(5);
+        amp.setTuner(tuner);
+    }
+
+    public void endRadio() {
+        System.out.println("라디오 끄는중...");
+        tuner.off();
+        amp.off();
+    }
+}
